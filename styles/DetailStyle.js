7436f126ea3c17beb0b68d05a2b11cc14cc8d782
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2018-05-09 14:49:36 +0700

    Add style to detail screen

---
+++ styles/DetailStyle.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#ffffff"
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: "600",
+        marginBottom: 5
+    },
+    tag : {
+        color: "#999999",
+        fontWeight: "800",
+        marginBottom: 10
+    },
+    content: {
+        margin: 20
+    },
+    des : {
+        color: "#555555",
+        fontWeight: "200",
+        marginBottom: 5
+    },
+    
+});
