e88b74af29b085f83a6bfcb1973474ae04aac338
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2018-05-09 14:49:22 +0700

    Add picker station list

--- screens/detail.js
+++ screens/detail.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Image } from 'react-native';
-import { Container, Text, Header, Title, Body } from 'native-base';
+import { View, Image, Picker} from 'react-native';
+import { Container, Text, Header, Title, Body, Button } from 'native-base';
+import styles from '../styles/DetailStyle';
 
 
 class Detail extends React.Component {
@@ -15,7 +16,9 @@ class Detail extends React.Component {
 
         this.state = {
             data:[],
-            stationList: []
+            stationList: [],
+            pricetime: [],
+            selected: ''
         }
 
     }
@@ -23,17 +26,35 @@ class Detail extends React.Component {
     componentDidMount(){
         this.setState({
             data: this.props.navigation.state.params.placeData,
-            stationList: this.props.navigation.state.params.stationListData
+            stationList: this.props.navigation.state.params.stationListData,
+            pricetime: this.props.navigation.state.params.pricetimeData
         })
+    }
+
+    loadStationList(){
+        return this.state.stationList.map(data => (
+            <Picker.Item label={data.value} value={data.value} key={data.value} />
+         ))
     }
 
     render(){
         return(
-            <Container>
-                <View>
-                    <Text>{this.state.data.name}</Text>
-                    <Text note>{this.state.data.station}</Text>
+            <Container style={styles.container}>
+            {/* <Image source={{uri: this.state.data.img}} style={{height:300, width: null}}/> */}
+                <View style={styles.content}>
+                    <Text style={styles.title}>{this.state.data.name}</Text>
+                    <Text style={styles.tag}>BTS {this.state.data.station} station</Text>
                     <Text>{this.state.data.des}</Text>
+                    <Text>Please select depart station</Text>
+                    <Picker
+                        mode="dropdown"
+                        selectedValue={this.state.selected}
+                        onValueChange={(itemValue, itemIndex) => this.setState({selected: itemValue})}>
+                        {this.loadStationList()}
+                    </Picker>
+                    <Button dark>
+                        <Text>Calcualte Price and Time</Text>
+                    </Button>
                 </View>
             </Container>
         )
